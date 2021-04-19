edc6651cf7b75041d63423060a4af59e3ac19c7f
M Ibrahim <m.ibrahim0798@gmail.com>
2021-04-19 17:01:37 +0500

    Bug Fix

--- Vita-FTPI-Core/Program.cs
+++ Vita-FTPI-Core/Program.cs
@@ -340,7 +340,7 @@ namespace Vita_FTPI_Core
                 {
                     if (file.Length.Equals(new FileInfo(Path.Combine(directory2.FullName, file.Name)).Length))
                     {
-                        if (!UnsafeCompare(GetHashSha256(file.FullName), GetHashSha256(Path.Combine(directory2.FullName, file.Name))))
+                        if (!Enumerable.SequenceEqual(GetHashSha256(file.FullName), GetHashSha256(Path.Combine(directory2.FullName, file.Name))))
                         {
                             Console.WriteLine("Copying file " + file.Name);
                             file.CopyTo(Path.Combine(directory2.FullName, file.Name), true);
@@ -355,27 +355,6 @@ namespace Vita_FTPI_Core
                         file.CopyTo(Path.Combine(directory2.FullName, file.Name), true);
                     }
                 }
-            }
-        }
-
-        // Copyright (c) 2008-2013 Hafthor Stefansson
-        // Distributed under the MIT/X11 software license
-        // Ref: http://www.opensource.org/licenses/mit-license.php.
-        static unsafe bool UnsafeCompare(byte[] a1, byte[] a2)
-        {
-            if (a1 == a2) return true;
-            if (a1 == null || a2 == null || a1.Length != a2.Length)
-                return false;
-            fixed (byte* p1 = a1, p2 = a2)
-            {
-                byte* x1 = p1, x2 = p2;
-                int l = a1.Length;
-                for (int i = 0; i < l / 8; i++, x1 += 8, x2 += 8)
-                    if (*((long*)x1) != *((long*)x2)) return false;
-                if ((l & 4) != 0) { if (*((int*)x1) != *((int*)x2)) return false; x1 += 4; x2 += 4; }
-                if ((l & 2) != 0) { if (*((short*)x1) != *((short*)x2)) return false; x1 += 2; x2 += 2; }
-                if ((l & 1) != 0) if (*((byte*)x1) != *((byte*)x2)) return false;
-                return true;
             }
         }
 
